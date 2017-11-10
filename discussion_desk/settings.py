c156827b2cbb5249e72639bc9a9fd839473290fd
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2017-11-10 15:22:40 +0530

    Final commit

--- discussion_desk/settings.py
+++ discussion_desk/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'discussion_desk.urls'
@@ -142,3 +143,5 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static_cdn'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
